6c7d3749984d54e7c12e2981f641d2b0dfef0e86
akusu2841-design <akusu2841@gmail.com>
2026-06-14 19:22:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 const joinBtn = document.getElementById("joinBtn");
 joinBtn.addEventListener("click",function(){
-  window.location.href="register.html";
+  window.location.href="register.html";});
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 
