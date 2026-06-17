3015da663c2391a98707363b9407e8d77e3e3b6a
akusu2841-design <akusu2841@gmail.com>
2026-06-17 10:11:04 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const loginBtn = document.getElementById("loginBtn");
+const showPassword = document.getElementById("showPassword");
+const rememberMe = document.getElementById("rememberMe");
+
+loginBtn.addEventListener("click",function{
+  if(name.length>3)
+});
