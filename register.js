1989c1aa3bee7d0343b7e29767a8240c3713e73a
akusu2841-design <akusu2841@gmail.com>
2026-06-22 18:28:12 +0100

    Update register.js

--- register.js
+++ register.js
@@ -26,7 +26,7 @@ form.addEventListener("submit", function(e){
       "• One number\n" +
       "• One special character"
     );
-    return;
+    redirect(window.href="login.html)";
   }
 
   alert("Registration successful!");
